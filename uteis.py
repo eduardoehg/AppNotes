017d36c46f22664cb1491596c4dee2d13100c2e1
Eduardo Henrique <eduardoehg@outlook.com>
2023-08-23 14:45:41 -0300

    Adicionado funcionalidade de gerar pdf e word

--- uteis.py
+++ uteis.py
@@ -1,4 +1,10 @@
 from tkinter import messagebox
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 
 def dimensionamento(janela, largura, altura, teto=0):
@@ -20,3 +26,56 @@ def msg_sucesso(msg):
 
 def msg_erro(msg):
     messagebox.showinfo(title='ERRO!', message=f'{msg}')
+
+
+def gerar_pdf(titulo, nota):
+    nome_arquivo = titulo + '.pdf'
+    documento = SimpleDocTemplate(nome_arquivo, pagesize=letter)
+    pdf = []
+
+    # Título centralizado
+    titulo_estilo = getSampleStyleSheet()["Title"]
+    titulo_paragrafo = Paragraph(titulo, titulo_estilo)
+    pdf.append(titulo_paragrafo)
+
+    # Adicionar espaço
+    pdf.append(Spacer(1, 20))
+
+    # Separar o texto em parágrafos
+    paragrafos = nota.split("\n")
+    texto_estilo = getSampleStyleSheet()["Normal"]
+    texto_estilo.alignment = 4
+    texto_estilo.leftIndent = 10
+    texto_estilo.rightIndent = 10
+
+    for paragrafo in paragrafos:
+        if paragrafo.strip():
+            elemento = Paragraph(paragrafo, texto_estilo)
+            pdf.append(elemento)
+            pdf.append(Spacer(1, 12))
+
+    documento.build(pdf)
+    msg_sucesso('PDF Gerado com Sucesso!')
+
+
+def gerar_word(titulo, nota):
+    documento = Document()
+
+    # Título centralizado
+    titulo_paragrafo = documento.add_paragraph()
+    titulo_run = titulo_paragrafo.add_run(titulo)
+    titulo_run.font.size = Pt(18)
+    titulo_paragrafo.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    # Adicionar espaço
+    documento.add_paragraph()
+
+    # Adicionar parágrafos justificados
+    paragrafos = nota.split("\n")
+    for paragrafo in paragrafos:
+        if paragrafo.strip():
+            p = documento.add_paragraph(paragrafo)
+            p.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+
+    documento.save(titulo + '.docx')
+    msg_sucesso('Documento Word Gerado com Sucesso!')
--- view/vizualizar_view.py
+++ view/vizualizar_view.py
@@ -1,4 +1,4 @@
-from uteis import dimensionamento
+from uteis import dimensionamento, gerar_pdf, gerar_word
 import tkinter as tk
 
 
@@ -7,10 +7,14 @@ class VizualizarView:
     def __init__(self):
 
         self.frame = None
+        self.titulo = None
+        self.nota = None
 
-    def tela_vizualizar_nota(self, frame, titulo, corpo):
+    def tela_vizualizar_nota(self, frame, titulo, nota):
         self.frame = frame
-        texto = f'Título: {titulo}\n\n{corpo}'
+        self.titulo = titulo
+        self.nota = nota
+        texto = f'Título: {self.titulo}\n\n{self.nota}'
 
         janela = tk.Toplevel(self.frame)
         largura, altura, x, y = dimensionamento(janela, 638, 670, 67)
@@ -26,15 +30,25 @@ class VizualizarView:
         frame_corpo.grid(row=1, column=0)
         frame_corpo.grid_propagate(False)
 
-        botao_pdf = tk.Button(frame_cabecalho, text='  Gerar PDF ', width=12)
+        botao_pdf = tk.Button(frame_cabecalho, text='  Gerar PDF  ', width=12, command=self.pdf)
         botao_pdf.config(bg='#111f32', fg='#a1d1d2', font=('Nunito', 12, 'bold'), relief='groove', borderwidth=0)
-        botao_pdf.grid(row=0, column=1, padx=(350, 5), pady=10)
+        botao_pdf.grid(row=0, column=1, pady=10)
+        botao_word = tk.Button(frame_cabecalho, text='  Gerar Word  ', width=12, command=self.word)
+        botao_word.config(bg='#111f32', fg='#a1d1d2', font=('Nunito', 12, 'bold'), relief='groove', borderwidth=0)
+        botao_word.grid(row=0, column=2, padx=(5, 220), pady=10)
+
         botao_voltar = tk.Button(frame_cabecalho, text='  Voltar  ', width=12, command=janela.destroy)
         botao_voltar.config(bg='#111f32', fg='#a1d1d2', font=('Nunito', 12, 'bold'), relief='groove', borderwidth=0)
-        botao_voltar.grid(row=0, column=2, pady=10)
+        botao_voltar.grid(row=0, column=3, pady=10)
 
         nota = tk.Text(frame_corpo, wrap='word', width=64, height=27)
         nota.config(font=('Nunito', 11, 'bold'), bg='#111f32', fg='#a1d1d2')
         nota.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
         nota.insert(1.0, texto)
         nota.config(state=tk.DISABLED)
+
+    def pdf(self):
+        gerar_pdf(self.titulo, self.nota)
+
+    def word(self):
+        gerar_word(self.titulo, self.nota)
